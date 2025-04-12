422926cc644607a644bc52082d4dce89310305e9
lucas lvn <lucas.lvn97439@gmail.com>
2025-04-12 10:27:34 +0200

    in public routes add /api/contract/list-ids

--- src/routes/public.ts
+++ src/routes/public.ts
@@ -3,4 +3,5 @@ export default [
   { method: "PATCH", path: "/api/auth/" },
   { method: "POST", path: "/api/auth/" },
   { method: "POST", path: "/api/contract/" },
+  { method: "GET", path: "/api/contract/list-ids" },
 ];
