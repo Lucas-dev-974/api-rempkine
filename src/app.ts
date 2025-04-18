52cd09cd55500b3af353269e8fa956060af42bc6
lucas lvn <lucas.lvn97439@gmail.com>
2025-04-18 10:06:30 +0200

    try bugfix

--- src/app.ts
+++ src/app.ts
@@ -9,30 +9,9 @@ import { AppDataSource } from "./data-source";
 import { JWTMiddleware } from "./middleware/JWT.middleware";
 import cors from "cors";
 
-// AppDataSource.initialize()
-//   .then(async () => {
-//     const app = express();
-
-//     const corsOptions = {
-//       origin: process.env.CORS_ORIGIN,
-//       optionsSuccessStatus: 200,
-//     };
-
-//     app.use(cors(corsOptions));
-
-//     const PORT = process.env.PORT || 8080;
-
-//     app.use(express.json());
-//     app.use(express.urlencoded({ extended: true }));
-//     app.use(JWTMiddleware.checkBearerToken);
-
-//     setRoutes(app);
-
-//     app.listen(PORT, () => {
-//       console.log(`Server is running on http://localhost:${PORT}`);
-//     });
-//   })
-//   .catch((error) => console.log(error));
+AppDataSource.initialize()
+  .then(async () => {})
+  .catch((error) => console.log(error));
 
 const app = express();
 
@@ -49,9 +28,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(JWTMiddleware.checkBearerToken);
 
-AppDataSource.initialize().then(() => {
-  setRoutes(app);
+// Middleware pour vérifier la connexion à la base de données
+app.use((req, res, next) => {
+  if (!AppDataSource.isInitialized) {
+    return res.status(503).json({ message: "Database not initialized" });
+  }
+  next();
 });
+
+setRoutes(app);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
