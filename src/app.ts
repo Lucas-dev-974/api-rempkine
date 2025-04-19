84488c48815954873eef175a7d557d95eeab3baa
lucas lvn <lucas.lvn97439@gmail.com>
2025-04-19 16:34:49 +0200

    try fix cors

--- src/app.ts
+++ src/app.ts
@@ -30,7 +30,10 @@ AppDataSource.initialize().then(() => {
 
   app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
+    );
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     next();
   });
