db40f5a7d73f04d9979a581ddfe0a54c3ba38eb1
lucas lvn <lucas.lvn97439@gmail.com>
2025-04-19 16:07:50 +0200

    try fix cors

--- src/app.ts
+++ src/app.ts
@@ -16,10 +16,15 @@ const corsOptions = {
   optionsSuccessStatus: 200,
 };
 
-app.all("*", function (req, res, next) {
+app.use(cors());
+
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
