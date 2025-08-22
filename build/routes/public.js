5297812574937ac75ef22c9d3466a5366d07abde
lucas lvn <lucas.lvn97439@gmail.com>
2025-08-22 21:53:19 +0200

    remove build

--- build/routes/public.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = [
-    // * Authentication
-    { method: "PATCH", path: "/api/auth/" },
-    { method: "POST", path: "/api/auth/" },
-];
-//# sourceMappingURL=public.js.map
