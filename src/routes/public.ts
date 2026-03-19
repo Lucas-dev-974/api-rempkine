9699580f8b28b87681eb00dfbc20c20f861713f6
Lucas lvn <lucas.lvn.dev@protonmail.com>
2026-03-19 06:01:21 +0100

    add public route "api/conntract/get-by-token

--- src/routes/public.ts
+++ src/routes/public.ts
@@ -2,12 +2,12 @@ export default [
   // * Authentication
   { method: "PATCH", path: "/api/auth/" },
   { method: "POST", path: "/api/auth/" },
-  { method: "POST", path: "/api/mail/send-contract" },
 
   // * Contract
+  { method: "POST", path: "/api/mail/send-contract" },
   { method: "POST", path: "/api/contract" },
   { method: "PATCH", path: "/api/contract" },
   { method: "POST", path: "/api/contract/list-ids" },
-  { method: "GET", path: "/api/contract/get-by-token" }
+  { method: "GET", path: "/api/contract/get-by-token" },
 ];
 
